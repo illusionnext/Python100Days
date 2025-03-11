772a7a2a8b759acc9b1a2a4b9c686dc5a8f3ef46
Farid <fargan@st.amu.edu.pl>
2025-03-12 00:42:06 +0100

    Band Name Generator

--- Day 1/task/task.py
+++ Day 1/task/task.py
@@ -1,1 +1,9 @@
-# Write your code below this line 👇
+import sys
+
+name = input("What is your name?").title()
+print(f"Hello {name}", file=sys.stderr, end="!\n")
+print(name)
+
+city = input("What is your favorite city?").strip().replace(" ", "")
+pet = input("What is your favorite pet name ever ?").strip().replace(" ", "")
+print(f"Your band name is {pet}{city} ! 🐈‍⬛🌆", file=sys.stderr, end="\n")
